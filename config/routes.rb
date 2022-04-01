a2dbdf5547d6e63cb842dad1fe6e448b09347392
Abdul Majeed <abdul.majeed@devsinc.com>
2022-04-01 16:44:37 +0500

    Setup home and about page

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'about', to: 'welcome#about'
+end
